4f75403a0b57cae9c0d8f074d23162782be5e3c8
Georgi Tyufekchiev <gtyufekchiev@gmail.com>
2023-10-01 22:29:42 +0300

    Add docker compose file. Flask -> Mongo connection fails for now.

--- 01.Docker-Compose-BERT/src/app.py
+++ 01.Docker-Compose-BERT/src/app.py
@@ -1,19 +1,26 @@
 import flask
 from bert_model import predict
+from pymongo import MongoClient
 
 app = flask.Flask(__name__, template_folder='templates')
 
+mongo_client = MongoClient("db",27017,username="admin",password="password",authSource="admin",authMechanism="SCRAM-SHA-256")  
+db = mongo_client["admin"] 
+try:
+    mongo_client.admin.command('ismaster')
+    print("Connected to MongoDB")
+except Exception as e:
+    print("MongoDB connection error:", str(e))
 
-# Set up the main route
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if flask.request.method == 'GET':
-        return (flask.render_template('main.html'))
+        return (flask.render_template('index.html'))
 
     if flask.request.method == 'POST':
         text = flask.request.form['text']
         result = predict(text)
-        return flask.render_template('main.html', result=result)
+        return flask.render_template('index.html', result=result)
 
 
 if __name__ == '__main__':
